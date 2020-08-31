fcd14fe631a9df75f33ef2a0fbde728b1d9c6a29
Yuenleung So <36447840+Subilan@users.noreply.github.com>
2020-08-31 14:44:08 +0800

    Update version to 16.2

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Downloading the latest paper build from paper api. (step 1/2)
-powershell Invoke-WebRequest https://papermc.io/api/v1/paper/1.16.1/latest/download -OutFile ./paper-latest.jar
+powershell Invoke-WebRequest https://papermc.io/api/v1/paper/1.16.2/latest/download -OutFile ./paper-latest.jar
 echo Downloading vanilla jar from BMCLAPI. (step 2/2)
 if not exist cache mkdir cache
-powershell Invoke-WebRequest https://bmclapi2.bangbang93.com/version/1.16.1/server -OutFile ./cache/mojang_1.16.1.jar
+powershell Invoke-WebRequest https://bmclapi2.bangbang93.com/version/1.16.2/server -OutFile ./cache/mojang_1.16.2.jar
 echo Done.
 if not "%1"=="nopause" pause

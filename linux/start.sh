ad9a2fced7ce183127f6e96c187a60d6d64d0658
Yuenleung So <36447840+Subilan@users.noreply.github.com>
2020-08-31 14:45:00 +0800

    Update version to 16.2 -linux

--- linux/start.sh
+++ linux/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo -e "\033[33mDownloading the latest paper build from paper api. (step 1/2)\033[0m"
-curl https://papermc.io/api/v1/paper/1.16.1/latest/download -o ./paper-latest.jar
+curl https://papermc.io/api/v1/paper/1.16.2/latest/download -o ./paper-latest.jar
 echo -e "\033[33mDownloading vanilla jar from BMCLAPI. (step 2/2)\033[0m"
 
 if [ ! -d "cache" ]; then
     mkdir cache
 fi
 
-curl https://bmclapi2.bangbang93.com/version/1.16.1/server -o ./cache/mojang_1.16.1.jar
+curl https://bmclapi2.bangbang93.com/version/1.16.2/server -o ./cache/mojang_1.16.2.jar
 
 echo -e "\033[32mDone.\033[0m"

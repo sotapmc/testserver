f34a7fa29e27c5a32c88461431e39fa0a207fac8
Yuenleung So <36447840+Subilan@users.noreply.github.com>
2021-05-01 21:27:12 +0800

    text

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Downloading the latest paper build from paper api. (step 1/2)
+echo Downloading paper 1.16.5 latest build from paper api. (step 1/2)
 powershell Invoke-WebRequest https://papermc.io/api/v1/paper/1.16.5/latest/download -OutFile ./paper-latest.jar
 echo Downloading vanilla jar from BMCLAPI. (step 2/2)
 if not exist cache mkdir cache
